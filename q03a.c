4ba66ffed9dcd47b7f713ed604c07a00eb4d49f4
isaias0rt0n <isaiasorton@gmail.com>
2021-02-25 18:22:15 -0300

    projeto final

--- q03a.c
+++ q03a.c
@@ -2,6 +2,7 @@
 Crie um programa em C que demonstre uma hierarquia de processos com o comando fork( ) -> Pai -> Filho ->Filho1.
 */
 
+/*Bibliotecas necessárias*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -10,10 +11,10 @@ Crie um programa em C que demonstre uma hierarquia de processos com o comando fo
 #include <string.h>
 
 int main(int argc, char *argv[]){
-    int pid;
+    int pid; //Inicializa variáveis
     int i;
-    pid = fork();
-    if (pid < 0) {
+    pid = fork(); //Cria um processo Filho
+    if (pid < 0) { //Verifica se ocorreu tudo certo com a criação
         printf("Vishh! Deu ruim\n");
         exit(1);
     }else{
@@ -25,17 +26,17 @@ int main(int argc, char *argv[]){
                 return(1);
             }else{
                 if (pid2 == 0) {
-                    write(1, "-> Filho1", 10);
+                    write(1, "-> Filho1", 10); //Mostra na tela. write(int fildes, const void *buf, size_t nbyte);
                     return(0);
                 }else{
-                    write(1, "-> Filho ", 9);
-                    wait(NULL);
+                    write(1, "-> Filho ", 9); //Escreve em tela
+                    wait(NULL); //Filho espera Filho1
                     return(0);
                 }
             }
         }else{
-            write(1, "Pai ", 4);
-            wait(NULL);
+            write(1, "Pai ", 4); //Escreve em tela 4 caracteres
+            wait(NULL); //Pai espera Filho
             printf(" -> FIM\n");
             return(0);
         }
--- q03b.c
+++ q03b.c
@@ -1,3 +1,7 @@
+/* Crie um programa que receba parâmetros na chamada do main e mostre na saída
+metade dos parâmetros pelo processo pai e a outra metade pelo processo filho*/
+
+//Bibliotecas necessárias
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -5,26 +9,26 @@
 int main(int argc, char *argv[]){
     int id;
 
-    id = fork();
-    if(id != 0){
+    id = fork(); //Cria processo filho
+    if(id != 0){ //Verifica se é o pai
         printf("Eu sou o PAI e espero o FILHO %d\n", id);
-        wait(0);
+        wait(0); //Espera Filho
         printf("O filho acabou\n");
         printf("Continua PAI\n");
-        for(int cont=(argc/2)+1; cont < argc; cont++){
+        for(int cont=(argc/2)+1; cont < argc; cont++){ //Percorre os argumentos da metade até o fim
             printf("%s -> ", argv[cont]);
         }
         
         printf("FIM\nO Pai acabou\n");
-    }else{
+    }else{ //Código do filho
         printf("Sou FILHO %d\n", getpid());
         //sleep(2);
 
-        for(int cont2=1; cont2<=(argc/2); cont2++){
+        for(int cont2=1; cont2<=(argc/2); cont2++){ //Percorre do primeiro argumento(sem ser o programa) até a metade
             printf("%s -> ", argv[cont2]);
         }
 
-        printf("FIM FILHO\nTerminei, agora vou embora...\n");
+        printf("FIM FILHO\nTerminei, agora vou embora...\n"); //Para sinalizar fim código do filho
     }
 
     return 0;
